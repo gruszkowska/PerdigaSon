6b8a7307fbfe4e874144426a6dd40e5d4ddeefda
gruszkowska <mariana.gru@hotmail.com>
2021-06-04 20:19:14 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://perdigason.herokuapp.com/'),
+    'url' => env('APP_URL', 'perdigason.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
